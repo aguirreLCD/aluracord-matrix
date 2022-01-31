0756dccb78ee8e49aac5ab5c2e9ee210ccb96aee
Lilian Daminelli Aguirre <lilian@Lilians-MacBook-Air.local>
2022-01-31 11:50:26 +0100

    deploy: vercel

--- pages/chat.js
+++ pages/chat.js
@@ -16,6 +16,8 @@ import { ButtonSendSticker } from '../src/components/ButtonSendSticker';
 //     }
 //   }
 
+
+// https://aluracord-matrix-tau-five.vercel.app/
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
